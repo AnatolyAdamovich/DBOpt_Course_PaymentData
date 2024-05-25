2141cd153af2ae6d66c2844404a2318ffaa2e1b7
Anatoly.s <tolik.frnk@yandex.ru>
2024-05-25 23:51:52 +0300

    README обновил & файлики подредактировал

--- solution/task1/optimal_indexes.sql
+++ solution/task1/optimal_indexes.sql
@@ -1,16 +1,17 @@
 -- Создание некластерного индекса на столбце CostByMaterial
-CREATE NONCLUSTERED INDEX IX_PaymentCategory_CostByMaterial
+CREATE NONCLUSTERED INDEX iPaymentCategory_CostByMaterial
 ON dbo.PaymentCategory (CostByMaterial)
 INCLUDE (Oid, Name, OptimisticLockField, GCRecord, ProfitByMaterial, NotInPaymentParticipantProfit)
 GO
 
 -- Создание некластерного индекса на столбце ProfitByMaterial  
-CREATE NONCLUSTERED INDEX IX_PaymentCategory_ProfitByMaterial
+CREATE NONCLUSTERED INDEX iPaymentCategory_ProfitByMaterial
 ON dbo.PaymentCategory (ProfitByMaterial)
 INCLUDE (Oid, Name, OptimisticLockField, GCRecord, CostByMaterial, NotInPaymentParticipantProfit)
 GO
 
--- Создание некластерного индекса на столбце iPayment_Date
+-- Создание некластерного индекса на столбце Date
 CREATE NONCLUSTERED INDEX iPayment_Date
 ON dbo.Payment (Date)
 
+-- возможно еще подошел бы iPaymentCategory_Name
